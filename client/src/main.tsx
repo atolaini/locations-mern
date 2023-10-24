2d580df8819e2b647604eefa5b8ad9c44251d474
atolaini <adam.tolaini@gmail.com>
2023-10-24 19:36:46 +0100

    starting working on router 6

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowerRouter, RouteProvider } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
 
-import { BrowserRouter } from 'react-router-dom';
+const router = createBrowerRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ client/src/shared/pages/Root.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import MainHeader from '../components/navigation/MainHeader';
+
+const Root = () => {
+  return <MainHeader />;
+};
+
+export default Root;
